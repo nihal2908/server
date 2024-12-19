03dc850b4d665e5ca500a5ecf4da92c1d1e12ebb
nihal2908 <nihalyadav8524@gmail.com>
2024-12-19 19:19:56 +0530

    first

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// handler for the root endpoint
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, `{"message": "Hello, World!"}`)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Method not allowed")
+	}
+}
+
+func main() {
+	// Register the handler for the root path
+	http.HandleFunc("/", helloHandler)
+
+	// Start the server on port 8080
+	fmt.Println("Server is running on http://localhost:3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
