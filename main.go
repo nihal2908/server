0f49e9ad0a4d88c7a26ec1ca5c8591b675a38f82
nihal2908 <nihalyadav8524@gmail.com>
2024-12-19 22:55:47 +0530

    12

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, `{"message": "Hello, World!" first change. second change. third change. forth change. fifth change. sixth change. seven. eight. nine. ten. 11. 12 }`)
+		fmt.Fprint(w, `{"message": "Hello, World!" first change. second change. third change. forth change. fifth change. sixth change. seven. eight. nine. ten. 11. 12 13}`)
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprint(w, "Method not allowed")
